26a910c3a416a6431044ed085252da88aeba9b99
TingchenFu <1607228797@qq.com>
2022-12-26 09:21:19 +0800

    evaluate trec file

---
+++ eval.py
@@ -0,0 +1,52 @@
+"""Approximately simulates trec_eval using pytrec_eval."""
+
+import argparse
+import os
+import sys
+
+import pytrec_eval
+
+
+def main():
+    run_file=sys.argv[1]
+    qrel_file=sys.argv[2]
+
+    assert os.path.exists(qrel_file)
+    assert os.path.exists(run_file)
+
+    with open(qrel_file, 'r') as f_qrel:
+        qrel = pytrec_eval.parse_qrel(f_qrel)
+
+    with open(run_file, 'r') as f_run:
+        run = pytrec_eval.parse_run(f_run)
+
+    evaluator = pytrec_eval.RelevanceEvaluator(
+        qrel, {"ndcg_cut.3","recip_rank"})
+
+    results = evaluator.evaluate(run)
+
+    def print_line(measure, scope, value):
+        print('{:25s}{:8s}{:.4f}'.format(measure, scope, value))
+
+    for query_id, query_measures in sorted(results.items()):
+        if query_id !='all':
+            continue
+        for measure, value in sorted(query_measures.items()):
+            print_line(measure, query_id, value)
+
+    # Scope hack: use query_measures of last item in previous loop to
+    # figure out all unique measure names.
+    #
+    # TODO(cvangysel): add member to RelevanceEvaluator
+    #                  with a list of measure names.
+    for measure in sorted(query_measures.keys()):
+        print_line(
+            measure,
+            'all',
+            pytrec_eval.compute_aggregated_measure(
+                measure,
+                [query_measures[measure]
+                 for query_measures in results.values()]))
+
+if __name__ == "__main__":
+    sys.exit(main())
